f6e886f4f169bf1bd5d694aaee3e57454c290aeb
Lukman10a <abdulrauflukman9@gmail.com>
2024-11-02 00:35:05 +0100

    update chat

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -7,7 +7,7 @@ import appstore from "../../public/svg/appstore.svg";
 export default function Chat() {
   return (
     <div className="flex flex-col items-center space-y-4">
-      <Image src={chat} alt="chat" />
+      <Image src={chat} alt="chat" className="md:w-52 sm:w-32" />
       <h1 className="text-6xl font-bold lg:text-5xl md:text-4xl sm:text-3xl text-center">
         <span className="text-[#27CD40]">Chat</span> with us directly on
         Homeland
@@ -16,9 +16,13 @@ export default function Chat() {
         A great platform to Build, Manage, Rent & lease your properties without
         any commisions.
       </p>
-      <div className="flex gap-4">
-        <Image src={googleplay} alt={"googleplay"} />
-        <Image src={appstore} alt={"appstore"} />
+      <div className="flex gap-4 ">
+        <Image
+          src={googleplay}
+          alt={"googleplay"}
+          className="md:w-52 sm:w-32"
+        />
+        <Image src={appstore} alt={"appstore"} className="md:w-52 sm:w-32" />
       </div>
       <Image src={iPhone} alt={"iPhone"} />
     </div>
