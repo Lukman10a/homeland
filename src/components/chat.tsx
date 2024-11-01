7933e0f64a7a1dd161dddc93e7c2f9e51673fa67
Lukman10a <abdulrauflukman9@gmail.com>
2024-11-02 00:48:26 +0100

    update chat

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -7,12 +7,12 @@ import appstore from "../../public/svg/appstore.svg";
 export default function Chat() {
   return (
     <div className="flex flex-col items-center space-y-4">
-      <Image src={chat} alt="chat" className="md:w-52 sm:w-32" />
+      <Image src={chat} alt="chat" className="md:w-52 sm:w-24" />
       <h1 className="text-6xl font-bold lg:text-5xl md:text-4xl sm:text-3xl text-center">
         <span className="text-[#27CD40]">Chat</span> with us directly on
         Homeland
       </h1>
-      <p className="text-xl font-medium sm:text-lg text-center">
+      <p className="text-xl font-medium sm:text-lg text-center sm:px-4">
         A great platform to Build, Manage, Rent & lease your properties without
         any commisions.
       </p>
@@ -22,9 +22,9 @@ export default function Chat() {
           alt={"googleplay"}
           className="md:w-52 sm:w-24"
         />
-        <Image src={appstore} alt={"appstore"} className="md:w-52 sm:w-24" />
+        <Image src={appstore} alt={"appstore"} className="lg:w-52 sm:w-24" />
       </div>
-      <Image src={iPhone} alt={"iPhone"} />
+      <Image src={iPhone} alt={"iPhone"} className="lg:w-2/3" />
     </div>
   );
 }
--- src/components/newsLetter.tsx
+++ src/components/newsLetter.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function NewsLetter() {
   return (
-    <div className="flex flex-col text-center gap-20 p-28 md:p-20 sm:p-10">
+    <div className="flex flex-col text-center gap-20 p-28 md:p-16 sm:p-4">
       <div className="space-y-4">
         <h1 className="text-[#000929] font-bold text-4xl sm:text-3xl">
           Subscribe to our Newsletter
@@ -13,13 +13,13 @@ export default function NewsLetter() {
         </p>
       </div>
       <div className="space-y-4">
-        <div className="flex items-center justify-center bg-[#F5FFF6] p-4 rounded-md max-w-lg mx-auto shadow-sm">
+        <div className="flex items-center justify-center bg-[#F5FFF6] p-4 rounded-md max-w-lg mx-auto shadow-sm ">
           <input
             type="email"
             placeholder="Enter your email address"
             className="p-3 rounded-l-md w-full focus:outline-none focus:ring-2 focus:ring-green-400"
           />
-          <button className="bg-[#27CD40] text-white px-10 py-3 rounded-md hover:bg-green-600">
+          <button className="bg-[#27CD40] text-white px-10 py-3 sm:px-6 rounded-md hover:bg-green-600">
             Submit
           </button>
         </div>
