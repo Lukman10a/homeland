ec298ad571658928e8e0fa01e188550efe5802be
Lukman10a <abdulrauflukman9@gmail.com>
2024-11-02 01:15:50 +0100

    update homepage

--- src/components/footer/buttomFooter.tsx
+++ src/components/footer/buttomFooter.tsx
@@ -6,7 +6,7 @@ import { FaLinkedin } from "react-icons/fa";
 
 export default function ButtomFooter() {
   return (
-    <footer className="flex justify-between items-center sm:flex-col sm:gap-8 border-t-2 border-[#E8E6F9] p-8 px-20 text-[#000929]">
+    <footer className="flex justify-between items-center text-center sm:flex-col sm:gap-8 border-t-2 border-[#E8E6F9] p-8 px-20 text-[#000929]">
       <p>©2024 Homeland. All rights reserved</p>
       <div className="flex gap-8">
         <FaFacebookF />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,37 @@
 // import Hero from "@/components/hero";
-
+import Image from "next/image";
+import transparency from "../../public/assets/transparency.png";
+import googleplay from "../../public/svg/googleplay.svg";
+import appstore from "../../public/svg/appstore.svg";
 export default function Home() {
-  return <div>{/* <Hero /> */}</div>;
+  return (
+    <div>
+      {/* <Hero /> */}
+      <div className="bg-[#27CD40] grid grid-cols-2 place-items-center lg:gap-4 p-20 md:p-10 sm:p-4">
+        <Image src={transparency} alt="transparency" />
+        <div className="text-white space-y-8 md:space-y-4 sm:space-y-2">
+          <h1 className="font-bold text-4xl md:text-3xl sm:text-2xl">
+            Experience our transparent budget spending process
+          </h1>
+          <p className="font-medium text-base">
+            designed to eliminate waste and promote efficiency. By incorporating
+            empiricism and lean thinking, we ensure your budget is optimized and
+            effectively managed throughout your project.
+          </p>
+          <div className="flex gap-4 ">
+            <Image
+              src={googleplay}
+              alt={"googleplay"}
+              className="md:w-32 sm:w-24"
+            />
+            <Image
+              src={appstore}
+              alt={"appstore"}
+              className="md:w-32 sm:w-24"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
