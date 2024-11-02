f013be10e2a10d8851af02e84a191dbd087fc9c4
Lukman10a <abdulrauflukman9@gmail.com>
2024-11-02 01:30:20 +0100

    update homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,13 @@ export default function Home() {
   return (
     <div>
       {/* <Hero /> */}
-      <div className="bg-[#27CD40] grid grid-cols-2 place-items-center lg:gap-4 p-20 md:p-8 sm:p-2">
+      <div className="bg-[#27CD40] grid grid-cols-2 sm:grid-cols-1 place-items-center lg:gap-6 p-20 md:p-10 sm:p-4">
         <Image src={transparency} alt="transparency" />
         <div className="text-white space-y-8 md:space-y-4 sm:space-y-2">
-          <h1 className="font-bold text-4xl md:text-2xl sm:text-base">
+          <h1 className="font-bold text-4xl md:text-2xl ">
             Experience our transparent budget spending process
           </h1>
-          <p className="font-medium text-base sm:text-xs">
+          <p className="font-medium text-base ">
             designed to eliminate waste and promote efficiency. By incorporating
             empiricism and lean thinking, we ensure your budget is optimized and
             effectively managed throughout your project.
@@ -22,12 +22,12 @@ export default function Home() {
             <Image
               src={googleplay}
               alt={"googleplay"}
-              className="md:w-32 sm:w-12"
+              className="md:w-32 sm:w-24"
             />
             <Image
               src={appstore}
               alt={"appstore"}
-              className="md:w-32 sm:w-12"
+              className="md:w-32 sm:w-24"
             />
           </div>
         </div>
